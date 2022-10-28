4aad171a904bdbd1bb33b6482ee1d61ed52e3f3b
tathagatamishra <tathagatamishra007@gmail.com>
2022-10-28 17:06:59 +0530

    going on

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,17 +1,22 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
+
 const bookSchema = new mongoose.Schema( {
     name: String,
-    author_id: {
+    authorId: {
         type: ObjectId,
-        ref: "Author"
+        ref: "newAuthor"
     }, 
     price: Number,
-    ratings: Number
+    ratings: Number,
+    publisher: {
+        type: ObjectId,
+        ref: "newPublisher"
+    }
 
 
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('LibraryBook', bookSchema)
+module.exports = mongoose.model('newBook', bookSchema)
