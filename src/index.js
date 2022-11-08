443ef60bcddf16656e80593ce1deac23bd0e8cf0
tathagatamishra <tathagatamishra007@gmail.com>
2022-11-08 18:40:37 +0530

    Working

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+
+const route = require('./routes/route.js');
+
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://new_user:jk1BBWwmxQpZ31zO@cluster0.pxvwsjp.mongodb.net/Axios", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-// app.use (
-//     function (req, res, next) {
-//         console.log ("inside GLOBAL MW");
-//         next();
-//   }
-//   );
-
 app.use('/', route);
 
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen(4000, function () {
+    console.log('Express app running on port 4000')
 });
---
+++ src/models/model.js
@@ -0,0 +1,11 @@
+
+
+// const mongoose = require('mongoose');
+
+// const < var name > = new mongoose.Schema( {
+
+
+// }, { timestamps: true });
+
+
+// module.exports = mongoose.model( < 'model name' >, < var name > )
